69f97944c79ce6af5e005fb1c8e7a29b48731398
Touseef Ahmad <ahmadtouseef946@gmail.com>
2025-07-13 14:31:24 +0500

    Remove example directory
    It was not in use so that's why we remove the example

--- examples/react-app-demo/index.js
+++
@@ -1,6 +0,0 @@
-import { setDevId, log, debug } from "dev-id-logger";
-
-setDevId(process.env.REACT_APP_DEV_ID);
-
-log("My debug log");
-debug("Breakpoint only for me"); // Only triggers if dev ID matches
